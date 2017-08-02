eafac2d4e5e1f8ffa80419cc4a470e10f9ca7aba
Aaron Astle <aaronpastle@gmail.com>
2017-08-01 21:46:45 -0700

    Adding base files for project

---
+++ README.md
@@ -0,0 +1,16 @@
+# Movie Trailer Website
+## Forked from Udacity ud036_StarterCode
+
+Source code for a Movie Trailer website project.
+
+My favorite movies are stored in a list of objects in `fav_movies.py`.  This 
+list is created through the use of the `Movie()` class in the `media.py`.
+
+To create a dynamically generated `*.html` file, in order to view in browsers, 
+you must run the following command.
+
+`$ python fav_movies.py`
+
+This will take the list of movies, create the web page, and open a browser window.
+
+
---
+++ media.py
@@ -0,0 +1,19 @@
+import webbrowser
+
+class Movie():
+    """Movie Class - Basic structure of the Movies
+
+    movie_title     (str): String of Movie Title
+    movie_storyline (str): String containing a short description of the movie
+    poster_image    (str): String of URL to movie poster
+    trailer_youtube (str): String of URL to youtube trailer
+    """
+    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
+
