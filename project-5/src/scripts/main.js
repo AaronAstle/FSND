1cc94c4984407ee1dd7a4dd3e3e23e09583e4099
Aaron Astle <thebionicknee@gmail.com>
2018-03-03 18:44:12 -0800

    progress check in

--- project-5/src/scripts/main.js
+++ project-5/src/scripts/main.js
@@ -1,8 +1,6 @@
 // Data Model
 function initMap() {
 
-  var markers = [];
-
   mapConfigOptions = {
     center: { lat: 38.575997, lng: -121.494923 },
     zoom: 13,
@@ -12,17 +10,10 @@ function initMap() {
 
   var map = new google.maps.Map(document.getElementById('map'), mapConfigOptions);
 
-  // Create a searchbox in order to execute a places search
-  var searchBox = new google.maps.places.SearchBox(
-    document.getElementById('places-search'));
-
-  // Bias the searchbox to within the bounds of the map.
-  searchBox.setBounds(map.getBounds());
-
-  var locations = [
+  var locationData = [
     {
-      title: "Nugget Market - Best Coffee Ever",
-      location: {
+      title: "Nugget Market",
+      coords: {
         lat: 38.5512412,
         lng: -121.537613
       },
@@ -30,7 +21,7 @@ function initMap() {
     },
     {
       title: "Mike's Bikes of Sacramento",
-      location: {
+      coords: {
         lat: 38.5801342,
         lng: -121.489208
       },
@@ -38,7 +29,7 @@ function initMap() {
     },
     {
       title: "LowBrau - Pub",
-      location: {
+      coords: {
         lat: 38.576069,
         lng: -121.482683
       },
@@ -46,15 +37,15 @@ function initMap() {
     },
     {
       title: "Golden 1 Center",
-      location: {
+      coords: {
         lat: 38.5810192,
         lng: -121.501754
       },
       type: "entertainment"
     },
     {
-      title: "Selland's Market Cafe and Bike Dog Brewing",
-      location: {
+      title: "Selland's Market Cafe",
+      coords: {
         lat: 38.564455,
         lng: -121.500921
       },
@@ -62,7 +53,7 @@ function initMap() {
     },
     {
       title: "Old Sacramento",
-      location: {
+      coords: {
         lat: 38.582400,
         lng: -121.505903
       },
@@ -70,7 +61,7 @@ function initMap() {
     },
     {
       title: "Raley Field",
-      location: {
+      coords: {
         lat: 38.581514,
         lng: -121.513459
       },
@@ -78,26 +69,130 @@ function initMap() {
     },
     {
       title: "Ace of Spades",
-      location: {
+      coords: {
         lat: 38.572353,
         lng: -121.4929031
       },
       type: "entertainment"
     }
-
   ];
 
 
-  locations.forEach( function(d) {
+  var Location = function(data) {
+    var self = this;
+    self.name = data.title;
+    self.lat = data.coords.lat;
+    self.lng = data.coords.lng;
+    self.URL = "";
+    self.street = "";
+    self.city = "";
+    self.phone = "";
 
-    var marker = new google.maps.Marker( {
-      position: d.location,
-      map: map,
-      title: d.title
+    this.visible = ko.observable(true);
+
+    var foursquareURL = 'https://api.foursquare.com/v2/venues/search?ll='+ this.lat + ',' + this.lng + '&client_id=' + clientID + '&client_secret=' + clientSecret + '&v=20160118' + '&query=' + this.name;
+
+    $.getJSON(foursquareURL).done(function(data) {
+      var results = data.response.venues[0];
+      console.log(results);
+      self.street = results.location.formattedAddress[0];
+      self.city = results.location.formattedAddress[1];
+      self.phone = results.contact.phone;
+      if (typeof self.phone === 'undefined'){
+        self.phone = "";
+      } else {
+        console.log(self.phone);
+        // self.phone = formatPhone(self.phone);
+      }
+    }).fail(function() {
+      alert("There was an error with the Foursquare API call. Please refresh the page and try again to load Foursquare data.");
     });
 
-    marker.setMap(map);
+    this.contentString = '<div class="info-window-content"><div class="title"><b>' + self.name + "</b></div>" +
+      '<div class="content"><a href="' + self.URL +'">' + self.URL + "</a></div>" +
+      '<div class="content">' + self.street + "</div>" +
+      '<div class="content">' + self.city + "</div>" +
+      '<div class="content">' + self.phone + "</div></div>";
 
-  });
+    this.infoWindow = new google.maps.InfoWindow({content: self.contentString});
 
+    this.marker = new google.maps.Marker({
+      position: new google.maps.LatLng(self.lat, self.lng),
+      map: map,
+      title: self.name
+    });
+
+    this.showMarker = ko.computed(function() {
+      if(this.visible() === true) {
+        this.marker.setMap(map);
+      } else {
+        this.marker.setMap(null);
+      }
+      return true;
+    }, this);
+
+    this.marker.addListener('click', function(){
+      self.contentString = '<div class="info-window-content"><div class="title"><b>' + self.name + "</b></div>" +
+        '<div class="content"><a href="' + self.URL +'">' + self.URL + "</a></div>" +
+        '<div class="content">' + self.street + "</div>" +
+        '<div class="content">' + self.city + "</div>" +
+        '<div class="content"><a href="tel:' + self.phone +'">' + self.phone +"</a></div></div>";
+
+      self.infoWindow.setContent(self.contentString);
+
+      self.infoWindow.close();
+      self.infoWindow.open(map, this);
+
+      self.marker.setAnimation(google.maps.Animation.BOUNCE);
+      setTimeout(function() {
+        self.marker.setAnimation(null);
+      }, 2100);
+    });
+
+    this.bounce = function(place) {
+      google.maps.event.trigger(self.marker, 'click');
+    };
+  };
+
+  function AppViewModel() {
+    var self = this;
+
+    this.locationFilter = ko.observable("");
+
+    this.locationList = ko.observableArray([]);
+
+    // Foursquare API settings
+    clientID = "BSXQ0ZCOWSV42AUZY1WQAW5RVFPNNZKZV5DZWMB11GG0DON0";
+    clientSecret = "SBIC5HKHK5V3SITRVQ45A3CZBBTBVUZED1QRM5BGFZVLDZGD";
+
+    locationData.forEach(function(locationItem){
+      self.locationList.push( new Location(locationItem));
+    });
+
+    this.filteredList = ko.computed( function() {
+      var filter = self.locationFilter().toLowerCase();
+      if (!filter) {
+        self.locationList().forEach(function(locationItem){
+          locationItem.visible(true);
+        });
+        return self.locationList();
+      } else {
+        return ko.utils.arrayFilter(self.locationList(), function(locationItem) {
+          var string = locationItem.name.toLowerCase();
+          var result = (string.search(filter) >= 0);
+          locationItem.visible(result);
+          return result;
+        });
+      }
+    }, self);
+
+    this.mapElem = document.getElementById('map');
+    this.mapElem.style.height = window.innerHeight - 50;
+  }
+
+  function errorHandling() {
+    alert("Google Maps has failed to load. Please check your internet connection and try again.");
+  }
+
+  ko.applyBindings(new AppViewModel());
 }
